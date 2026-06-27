1bc6d0ac640af057f73a6ad150a09bcf27a99576
ExposureMG <exposuremg@protonmail.com>
2026-06-27 21:31:47 +0100

    basic command handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,40 @@
 #include <dpp/dpp.h>
-#include <dotenv.h>
+#include <include/dotenv.hpp>
  
 int main() {q
     dotenv::init();
     dpp::cluster bot(dotenv::getenv("TOKEN"));
 
-    bot.on_log(&dpp::utility::logcallback);
-
-    bot.on_message_create([&bot](const dpp::message_create_t &event){});
-
-    bot.start(dpp::st_return);
+	bot.on_log(dpp::utility::cout_logger());
+	 
+	/* The event is fired when someone issues your commands */
+    bot.on_slashcommand([&bot](const dpp::slashcommand_t & event) {
+	    /* Check which command they ran */
+	    if (event.command.get_command_name() == "ping") {
+            event.reply("Pong!");
+	    } else if (event.command.get_command_name() == "pong") {
+	        event.reply("Ping!");
+        } else if (event.command.get_command_name() == "ding") {
+	        event.reply("Dong!");
+	    } else if (event.command.get_command_name() == "dong") {
+            event.reply("Ding!");
+        }
+	});
+	 
+	bot.on_ready([&bot](const dpp::ready_t & event) {
+	    if (dpp::run_once<struct register_bot_commands>()) {
+	        /* Create some commands */
+	        dpp::slashcommand pingcommand("ping", "Pong!", bot.me.id);
+	        dpp::slashcommand pongcommand("pong", "Ping!", bot.me.id);
+            dpp::slashcommand dingcommand("ding", "Dong!", bot.me.id);
+	        dpp::slashcommand dongcommand("dong", "Ding!", bot.me.id);
+	 
+	        /* Register our commands in a list using bulk */
+	        bot.guild_bulk_command_create({ pingcommand, pongcommand, dingcommand, dongcommand }, dotenv::getenv("GUILD_ID"));
+        }
+	});
+	 
+	bot.start(dpp::st_wait);
+	 
+	return 0;
 }
